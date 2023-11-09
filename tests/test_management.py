a0e8726d696e467edcc5edf4d4649415682b7966
MFA-X-AI <fahreza@xpress.ai>
2023-11-09 12:20:31 +0700

    add usage metrics test

--- tests/test_management.py
+++ tests/test_management.py
@@ -161,3 +161,19 @@ def test_delete_vector_space():
     id_exists = any(vecto_vector_space.id == test_vector_space.id for vecto_vector_space in updated_vs_list)
     logger.info("Check if the vector space is deleted.")
     assert not id_exists
+
+
+@pytest.mark.metrics
+class TestMetrics:
+
+    # Test getting monthly usage from Vecto
+    def test_usage(self): 
+        from datetime import datetime
+        today = datetime.now()
+        usage_response = user_vecto.usage(user_vecto.vector_space_id, today.year, today.month)
+        logger.info("Checking that usage returns a valid response.")
+        assert usage_response is not None
+        logger.info("Checking that usage for today is not empty.")
+        assert usage_response.usage.lookups.dailyMetrics[today.day-1].count > 0
+        assert usage_response.usage.indexing.dailyMetrics[today.day-1].count > 0
+
