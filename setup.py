86e2b5d3bcdd3c81f8340d9592c76cdd4e2f1a66
MFA-X-AI <fahreza@xpress.ai>
2024-01-09 12:09:56 +0800

    add pypi project desc

--- setup.py
+++ setup.py
@@ -1,11 +1,33 @@
 from setuptools import setup
 
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='vecto-sdk',
-    version='0.2.1',
+    version='0.2.2',
+    author='Xpress AI',
+    author_email='eduardo@xpress.ai',
+    description='Official Python SDK for Vecto',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/XpressAI/vecto-python-sdk',
     packages=['vecto'],
     install_requires=[
         'requests',
         'requests_toolbelt'
-    ]
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.8',
+    keywords='vector-database, vector-search',
+    project_urls={
+        'Documentation': 'https://docs.vecto.ai/',
+        'Source': 'https://github.com/XpressAI/vecto-python-sdk',
+        'Issues': 'https://github.com/XpressAI/vecto-python-sdk/issues'
+    },
 )
+
